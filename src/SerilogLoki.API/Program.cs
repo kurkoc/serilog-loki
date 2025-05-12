6ce73bdc920617b33173d204f037cd9aac169ccd
kursad.koc <kursadkoc@live.com>
2025-05-12 20:57:24 +0300

    update

--- src/SerilogLoki.API/Program.cs
+++ src/SerilogLoki.API/Program.cs
@@ -1,25 +1,46 @@
 using Serilog;
+using Serilog.Enrichers.Span;
+using Serilog.Events;
+using Serilog.Formatting.Json;
 using Serilog.Sinks.Grafana.Loki;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Host.UseSerilog((context, configuration) =>
+builder.Host.UseSerilog((context, serviceProvider, loggerConfiguration) =>
 {
-    configuration
-    .MinimumLevel.Information()
-    .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Warning)
-    .MinimumLevel.Override("System", Serilog.Events.LogEventLevel.Warning)
-    .Enrich.FromLogContext()
-    .Enrich.WithProperty("environment", context.HostingEnvironment.EnvironmentName)
-    .Enrich.WithProperty("application", context.HostingEnvironment.ApplicationName)
-    .Enrich.WithProperty("machine", Environment.MachineName)
-    .WriteTo.Console()
-    .WriteTo.GrafanaLoki("http://localhost:3100", labels:
-    [
-        new LokiLabel { Key = "application", Value = context.HostingEnvironment.ApplicationName },
-        new LokiLabel { Key = "environment", Value = context.HostingEnvironment.EnvironmentName },
-        new LokiLabel { Key = "machine", Value = Environment.MachineName }
-    ]);
+    //TODO : get configuration
+    string lokiUrl = "http://localhost:3100";
+    string logLevelAsString = "Information";
+
+    var level = Enum.TryParse<LogEventLevel>(logLevelAsString, true, out var logLevel)
+        ? logLevel
+        : LogEventLevel.Information;
+
+
+    loggerConfiguration
+        .Enrich.WithClientIp()
+        .Enrich.WithSpan()
+        .Enrich.WithCorrelationId()
+        .Enrich.FromLogContext()
+        .Enrich.WithProperty("environment", context.HostingEnvironment.EnvironmentName)
+        .Enrich.WithProperty("application", context.HostingEnvironment.ApplicationName)
+        .Enrich.WithProperty("machine", Environment.MachineName)
+        .WriteTo.Console(new JsonFormatter(renderMessage: true));
+
+    loggerConfiguration
+        .MinimumLevel.Is(level)
+        .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+        .MinimumLevel.Override("System", LogEventLevel.Warning);
+
+    if (!context.HostingEnvironment.IsDevelopment())
+    {
+        loggerConfiguration.WriteTo.GrafanaLoki(lokiUrl, labels:
+        [
+            new LokiLabel { Key = "application", Value = context.HostingEnvironment.ApplicationName },
+            new LokiLabel { Key = "environment", Value = context.HostingEnvironment.EnvironmentName },
+            new LokiLabel { Key = "machine", Value = Environment.MachineName }
+        ]);
+    }
 });
 
 var app = builder.Build();
